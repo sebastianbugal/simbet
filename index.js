927b4fdf01454663a4bd12f2125470400bd0d287
sebastianbugal <49225786+sebastianbugal@users.noreply.github.com>
2020-08-04 14:10:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,12 +32,9 @@ var session=ses ( {
 	resave: false,
 	saveUninitialized: true
 } );
-// io.set('transports', [
-//     'websocket'
-//   , 'flashsocket'
-//   , 'htmlfile'
-//   , 'jsonp-polling'
-//   ]);
+io.set('transports', [
+    'websocket'
+  ]);
 
 app.use( session );
 io.use( function ( socket, next ) {
